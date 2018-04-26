78ba6c34556970829fd1b0dfe35c0142165864a1
Duncan <marjoribanks.d@gmail.com>
2018-04-26 15:41:01 +0100

    set up a single instance of each class for testing purposes

--- console.rb
+++ console.rb
@@ -3,14 +3,23 @@ require_relative("models/movie")
 require_relative("models/casting")
 
 require("pry-byebug")
-star1 = Star.new({'first_name' => 'Postman', 'last_name' => 'Pat'})
+
+
+star1 = Star.new({
+  'first_name' => 'Postman',
+  'last_name' => 'Pat'})
 star1.save()
 
+movie1 = Movie.new({
+   "title" => "Die Hard 7 - Die Hard the mail's late",
+   "genre" => "Childrens",
+   "rating" => "8"})
+movie1.save()
 
-
-
-
-
+casting1 = Casting.new({
+  "movie_id" => movie1.id,
+  "star_id" => star1.id})
+casting1.save()
 
 
 
